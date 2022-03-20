8c199023f41f576e39527ac09c51d11ad4e67044
Jean-Francois Castel-Branco <ecsalt@gmail.com>
2022-03-20 20:05:32 +0100

    refactored and shape + rotation tweens implemented

--- js/world.js
+++ js/world.js
@@ -12,35 +12,47 @@ import { Vector3 } from 'three'
 export class World {
 
     constructor () {
-        
-        this.trigger;
-        this.tweenInit;
-        this.onMouseMove;
-        this.onScroll;
-        this.update;
-        this.loop;
+    
+        /* ---------------------- ROTATION ---------------------- */
+        this.tw_time_rotation = 500;
+        this.tw_easing_rotation = TWEEN.Easing.Exponential.Out;
+        this.tw_rotation = new Vector3 (0, 0, 0);
+        this.tw_rotation_array = [
+            new Vector3 ( -0.7, 0.5, 0.1 ),
+            new Vector3 ( 0.6, -0.8, 0.4 ),
+            new Vector3 ( 0.5, 0.2, 0.9 )
+        ];
 
-        this.result;
-        this.info;
+        /* ---------------------- SHAPE ---------------------- */
+        this.tw_time_shape = 250;
+        this.tw_easing_shape = TWEEN.Easing.Quadratic.InOut;
+        this.tw_shape = new Vector3 (1, 1, 1);
+        this.tw_shape_array = [
+            new Vector3 ( 1.4, 1.1, 4 ),
+            new Vector3 ( 2, 2, 4 ),
+            new Vector3 ( 0.25, 0.25, 0.25 )
+        ];
 
-        this.tween_default_and_intro;
-        this.tween_intro_and_text;
-        this.tween_text_and_bottom;
-        this.tween_time = 500;
-        this.easing_default = TWEEN.Easing.Exponential.Out;
+        /* ---------------------- TWEEN ARRAYS ---------------------- */
+        this.tw_init_vectors = [ this.tw_rotation, this.tw_shape ];
+        this.tw_arrays = [ this.tw_rotation_array, this.tw_shape_array ];
+        this.tw_easings = [ this.tw_easing_rotation, this.tw_easing_shape ];
+        this.tw_times = [ this.tw_time_rotation, this.tw_time_shape ];
+
+        this.tweens = [[],[]];
 
         this.window_height = window.innerHeight;
         this.div = [
             'intro',
             'text',
             'miniweb'
-          ]
+        ];
         this.mouse = { x: 0, y: 0 };
         document.addEventListener ( 'mousemove', ( event ) => {
             event.preventDefault();
             this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
             this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-        } );
+        });
 
         this.speed = { x: 0, y: 0 };
         this.depth = { x: 1, y: 1, z: 1 };
@@ -87,19 +99,6 @@ export class World {
         this.dirLight = new THREE.PointLight(this.colours.orange, 1);
         this.loader = new GLTFLoader();
         this.color1 = new THREE.Color();
-        
-        // 3D KEYFRAMES
-        this.rotation_set = {
-            init: new Vector3 ( 0, 0, 0 ),
-            intro: new Vector3 ( -0.7, 0.5, 0.1 ),
-            text: new Vector3 ( 0.6, -0.8, 0.4 ),
-            bottom: new Vector3 ( 0.5, 0.2, 0.9 )
-        }
-
-        // VECTOR CONTAINERS
-        this.v = new Vector3 (0, 0, 0, 0);
-        // this.v = { x: 0, y: 0, rotation: 0 };
-        this.v_tmp = new Vector3 (0, 0, 0, 0);
     }
 
     init() {
@@ -132,12 +131,11 @@ export class World {
     loadModel() {
         this.loader.load(
             this.logoURL,
-            (glb) => {
-                console.log('glb:', glb)
-                this.logo.add(glb.scene)
-                this.scene.add(this.logo)
+            ( o ) => {
+                this.logo.add ( o.scene )
+                this.scene.add ( this.logo )
                 this.logo.traverse( ( child ) => {
-                    if (child instanceof THREE.Mesh) {
+                    if ( child instanceof THREE.Mesh ) {
                         child.material.color.setHex ( this.colours.orange );
                     }
                 })
@@ -146,59 +144,43 @@ export class World {
     }
 
     tweenInit() {
-        this.tween_default_and_intro = new TWEEN.Tween ( this.v )
-            .to ( this.rotation_set.intro, this.tween_time )
-            .easing ( this.easing_default )
-            .yoyo (true)
-            .repeat (1)
-            .onUpdate ( () => this.update() );
-        
-        this.tween_intro_and_text = new TWEEN.Tween ( this.v )
-            .to ( this.rotation_set.text, this.tween_time )
-            .easing ( this.easing_default )
-            .yoyo (true)
-            .repeat (1)
-            .onUpdate ( () => this.update() );
-
-        this.tween_text_and_bottom = new TWEEN.Tween ( this.v )
-            .to ( this.rotation_set.bottom, this.tween_time )
-            .easing ( this.easing_default )
-            .yoyo (true)
-            .repeat (1)
-            .onUpdate ( () => this.update() );
+        for (var i = 0; i < this.tw_arrays.length; i++) {
+            for (var j = 0; j < this.tw_arrays[i].length; j++ ) {
+                const tween = new TWEEN.Tween ( this.tw_init_vectors[i] )
+                    .to ( this.tw_arrays[i][j], this.tw_times[i] )
+                    .easing ( this.tw_easings[i] )
+                    .yoyo (true)
+                    .repeat (1)
+                    .onUpdate ( () => this.update() );
+                    this.tweens[i].push ( tween );
+            }
+        }
     }
 
     trigger ( string_array ) {
-        for (var i = 0; i < string_array.length; i++) {
-            const e = string_array[i];
-            const wp = new Waypoint ({
-                element: document.getElementById ( e ),
-                handler: ( d ) => {
-                    if ( e == string_array[0] ) {
-                        if ( d == 'down' ) {
-                            this.tween_default_and_intro.start();
-                            setTimeout ( () => { this.tween_default_and_intro.pause() }, this.tween_time );
-                        } else if ( d == 'up' ) {
-                            this.tween_default_and_intro.resume();
-                        }
-                    } else if ( e == string_array[1] ) {
-                        if ( d == 'down' ) {
-                            this.tween_intro_and_text.start();
-                            setTimeout ( () => { this.tween_intro_and_text.pause() }, this.tween_time );
-                        } else if ( d == 'up' ) {
-                            this.tween_intro_and_text.resume();
-                        }
-                    } else if ( e == string_array[2] ) {
-                        if ( d == 'down' ) {
-                            this.tween_text_and_bottom.start();
-                            setTimeout ( () => { this.tween_text_and_bottom.pause() }, this.tween_time );
-                        } else if ( d == 'up' ) {
-                            this.tween_text_and_bottom.resume();
-                        }
-                    }
-                },
-                offset: this.window_height - 10
-            });
+        for (var i = 0; i < this.tweens.length; i++) {
+            for (var j = 0; j < string_array.length; j++) {
+                const time = this.tw_times[i];
+                const e = string_array[j];
+                const tween = this.tweens[i][j];
+                if ( tween instanceof TWEEN.Tween ) {
+                    const timeout = () => { tween.pause() };
+                    const wp = new Waypoint ({
+                        element: document.getElementById ( e ),
+                        handler: ( d ) => {
+                            for (var i = 0; i < string_array.length; i++) {
+                                if ( e == string_array[i] ) {
+                                    if ( d == 'down' ) {
+                                        tween.start();
+                                        setTimeout ( timeout, time );
+                                    }
+                                }
+                                if ( d == 'up' ) { tween.resume() }
+                            }
+                        }, offset: this.window_height - 10
+                    });
+                }
+            }
         }
     }
 
@@ -212,17 +194,12 @@ export class World {
         window.onscroll = ( event ) => {
             this.scroll = window.scrollY / document.body.scrollHeight;
     
-            // this.depth.x = 1 + this.scroll * 1;
-            // this.depth.y = 1 + this.scroll * 0.5;
-            // this.depth.z = 1 + this.scroll * 5;
-    
-            this.rgb = this.scroll * 12 + 0.25;
+            this.rgb = ( ( this.scroll * 12 ) % 0.66 ) + 0.25;
             this.r = 1 - this.scroll * 12 + 0.25;
             this.g = ( this.scroll * 12 + 0.25 ) / 2;
             this.b = 0.5 + ( this.scroll * 12 + 0.25 ) / 2;
     
             this.scene.background.setRGB (this.rgb, this.rgb, this.rgb);
-            this.logo.scale.set (this.depth.x, this.depth.y, this.depth.z);
             this.color1.setRGB ( this.r, this.g, this.b );
             this.logo.traverse ( ( child ) => {
                 if (child instanceof THREE.Mesh) {
@@ -236,15 +213,19 @@ export class World {
         const id = requestAnimationFrame ( () => this.loop() );
         this.renderer.render ( this.scene, this.camera );
 
-        this.result = TWEEN.update();
-        // if (!result) cancelAnimationFrame (id);
+        TWEEN.update();
 
-        this.logo.rotation.x = this.v.y - this.mouse.y;
-        this.logo.rotation.y = this.v.x + this.mouse.x;
+        this.logo.rotation.x = this.tw_init_vectors[0].y - this.mouse.y;
+        this.logo.rotation.y = this.tw_init_vectors[0].x + this.mouse.x;
     }
 
     update() {
-        this.logo.rotation.x = this.v.y;
-        this.logo.rotation.y = this.v.x;
+        this.logo.rotation.x = this.tw_init_vectors[0].y;
+        this.logo.rotation.y = this.tw_init_vectors[0].x;
+        this.logo.scale.set ( 
+            this.tw_init_vectors[1].x, 
+            this.tw_init_vectors[1].y, 
+            this.tw_init_vectors[1].z 
+        );
     }
 }
